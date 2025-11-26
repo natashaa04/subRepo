2c266c94e1049b21b046cd04ed9bf7836b908337
natashaa04 <natashachawda370@gmail.com>
2025-11-26 10:26:08 +0530

    add another function

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-import express from "express"
+
 import { add, greet } from "./file2";
-
-
-const app = express()
-
-
 
 greet();
 console.log(add(1,2));
 
-app.listen(4000,()=>{
-    console.log("server running on port 4000")
-})
